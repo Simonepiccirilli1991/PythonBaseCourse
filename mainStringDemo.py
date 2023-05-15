81bf58a04af92a2695fda2b918758fd651edaa8e
Simonepiccirilli1991 <Kuugo91@gmail.com>
2023-05-15 20:29:36 +0200

    python harvard base course

---
+++ mainStringDemo.py
@@ -0,0 +1,12 @@
+
+# ask user input
+name = input("Write ur name")
+
+#Remove space
+name = name.strip()
+
+#Uppercase 1st letter
+name = name.capitalize()
+
+#nuova feature, va messo f davanti il print line
+print(f"Hello, {name}")
