a9819772fbfddd9b821056453a44b1236724581c
Simonepiccirilli1991 <Kuugo91@gmail.com>
2023-05-15 20:44:46 +0200

    course demo scrypt

--- mainStringDemo.py
+++ mainStringDemo.py
@@ -1,12 +1,18 @@
 
 # ask user input
-name = input("Write ur name")
+name = input("Write ur name").strip().title()
+
+#in python se un valore si splitta, o ne ha piu di 1 in response puoi assegnare piu variabili in input
+firstword, lastword = name.split(" ")
 
 #Remove space
-name = name.strip()
+#name = name.strip()
 
-#Uppercase 1st letter
-name = name.capitalize()
+#Uppercase 1st letter #Uppercare of every word
+#name = name.capitalize()
+
+#Uppercare of every word
+#name = name.title()
 
 #nuova feature, va messo f davanti il print line
-print(f"Hello, {name}")
+print(f"Hello, {firstword}")
