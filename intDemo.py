a878b4c24e4904c49943b8da9bc331b5b524c8fb
Simonepiccirilli1991 <Kuugo91@gmail.com>
2023-05-17 21:16:55 +0200

    adding int py base demo

---
+++ intDemo.py
@@ -0,0 +1,11 @@
+x = input("what ur numeber")
+y = input("what's ur second number")
+
+#cosi printa stringa, ogni input viene considerato stringa
+print(x+y)
+
+#Cosi gli stiamo dicendo che sara un numbero e printera la somma in modo corretto
+x = int(input("what's ur number ?"))
+y = int(input("whatsUrNUmber"))
+
+print(x + y)
