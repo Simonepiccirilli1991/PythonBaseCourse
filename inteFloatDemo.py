a1f46cb413399c4eab51a424eb23315710aa5299
Simonepiccirilli1991 <Kuugo91@gmail.com>
2023-05-25 23:21:43 +0200

    float demo added

--- inteFloatDemo.py
+++ inteFloatDemo.py
@@ -1,3 +1,4 @@
+#int
 x = input("what ur numeber")
 y = input("what's ur second number")
 
@@ -9,3 +10,13 @@ x = int(input("what's ur number ?"))
 y = int(input("whatsUrNUmber"))
 
 print(x + y)
+
+#float prende i decimali
+print("float time")
+x = float(input("what's ur number ?"))
+y = float(input("whatsUrNUmber"))
+
+#rouad arrotonda al numero piu vicino a l'intero
+z = round(x + y)
+#qui stiamo dicendo di formattare e mettere la , dopo ogni decimale3
+print(f"{z:,}")
