691393d8d3cd94878f71938040dace72697a1c21
Simonepiccirilli1991 <Kuugo91@gmail.com>
2023-06-02 13:02:54 +0200

    personal scrypt implementing

---
+++ personalScrypt.py
@@ -0,0 +1,17 @@
+input = input("Cosa vuoi fare?")
+
+
+# essenzialmente sto scypt deve fare da bot si le varie funzioni abilitate
+#scegliere al momento 3 funzioni e implementarle
+
+
+#cerca una lista gia definita di oggetti interessanti, se ne trova torna img link e prezzo
+def call_api_ebay():
+    ebay_resp = "TODO" #dovrei implementare una libreria per fare una chiamata esterna
+
+
+    #cerca una lista gia definita di oggetti interessanti, se ne trova torna img link e prezzo
+def check_api_amazon():
+    amazon_resp = "TODO"
+
+#TODO: integrare librerie per effettuare chiamate // vedere se possibile inserire py in java
