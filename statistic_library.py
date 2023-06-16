0b6fb2ed92e6baefe82d9f7eb51c36c6e28a59fb
Simonepiccirilli1991 <Kuugo91@gmail.com>
2023-06-17 00:29:18 +0200

    system library

---
+++ statistic_library.py
@@ -0,0 +1,5 @@
+import statistics
+
+#usiamo metodo che fa media dei valori interni alla lista passata
+media = statistics.mean([100,90,100,90])
+print(media)
---
+++ system_library.py
@@ -0,0 +1,16 @@
+import sys
+
+#prendo il secondo argomento perche il primo e in 0 dopo il comando per runnare python: ->  "python system_library.py Simone"
+try:
+    print("hello, my name is", sys.argv[1])
+except IndexError:
+    print("U don't have provided ur name")
+
+## metodo 2 senza usare eccezzione, ma solo con logica
+if len(sys.argv) < 2:
+    print("Too few argoment")
+elif len(sys.argv) > 2:
+    print("Too many arguments")
+else:
+    print("hello, my name is", sys.argv[1])
+# se si passa argomento cosi python system_library.py "Simone Piccirilli"  <- tutto quello "" viene preso come un solo argomento
