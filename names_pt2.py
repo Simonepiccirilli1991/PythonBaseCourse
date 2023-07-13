a216328d302e3306a84669c0520651f35206cde0
Simonepiccirilli1991 <Kuugo91@gmail.com>
2023-07-14 00:38:36 +0200

    continuing with csv reader lib

--- names_pt2.py
+++ names_pt2.py
@@ -10,3 +10,15 @@ with open("students.csv") as file:
 
 for student in sorted(students, key=lambda student: student["home"]):
     print(f"{student['name']} is in {student['home']}")
+
+#per rendere piu robusto  il tutto posso dichiarare le colonne e il relativo nome nel file csv:
+# home, name
+# wisley maison, ron
+# posso accedere con un csv dictionari e diventa cosi:
+with open("students.csv") as file:
+    reader = csv.DictReader(file)
+    for row in reader: #usando il DictReader e avendo dichiarato il nome dei campi del csv a l'inizio del file fosso farlo
+        students.append({"name": row["name"],"home": row["home"]}) #uso il dic per salvare oggetti in lista
+
+for student in sorted(students, key=lambda student: student["home"]):
+    print(f"{student['name']} is in {student['home']}")
