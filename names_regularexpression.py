71a126d17353945b87027b167b950a63c763b7b9
Simonepiccirilli1991 <Kuugo91@gmail.com>
2023-07-19 23:58:05 +0200

    improving reguar expression

--- names_regularexpression.py
+++ names_regularexpression.py
@@ -6,3 +6,10 @@ if re.search(r".+@.+\.edu", email):
     print("Valid")
 else:
     print("Invalid")
+
+#che diventa
+if re.search("^\w+@\w+\.edu$", email): #^ significa all'inizio della stringa, \w vuol dire che e accettata ogni parola compreso_,
+    #@ divide l'oggetto email , \w uguale, ci deve essere una parola az 0-9 _ per essere accettata e \.edu$ vuol dire che deve finire con .edu la stringa dopo la @
+    print("Valid")
+else:
+    print("Invalid")
