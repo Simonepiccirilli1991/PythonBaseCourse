1e5dddba1e47423ad9af9dce45ed85ccc0508f7c
Simonepiccirilli1991 <Kuugo91@gmail.com>
2023-08-09 20:59:40 +0200

    regular expression ended

--- twitter_regexpress.py
+++ twitter_regexpress.py
@@ -2,8 +2,12 @@ import re
 
 url = input("Ur Url: ").strip()
 #base , letteramente hardcodata
-username = url.replace("https://twitter.com/","")
+username = url.replace("https://twitter.com/", "")
 
-usr = re.sub(r"^(https?://)?(www\.)?twitter\.com","",url) #? vuol dire opzionale 0 o 1, \ e un escape epr indicare che il . e solo un .
+usr = re.sub(r"^(https?://)?(www\.)?twitter\.com", "", url) #? vuol dire opzionale 0 o 1, \ e un escape epr indicare che il . e solo un .
 #le parentesi servono a mettere tutto insieme
 print(usr)
+
+#final version
+if matches := re.search(r"^https?://(?:www\.)?twitter\.com/(.+)$", url, re.IGNORECASE):
+    print(f"Username:", matches.group(1))
