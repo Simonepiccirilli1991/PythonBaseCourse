d738583a478d1085e46ba401ec2a043f0282d864
Simonepiccirilli1991 <Kuugo91@gmail.com>
2023-08-28 23:25:52 +0200

    update oop

--- oop/students.py
+++ oop/students.py
@@ -5,7 +5,12 @@ def main():
 def get_student():
     name = input("Name: ")
     house = input("House: ")
-    return name,house #in questo caso stiamo tornando una tuple, un unico oggetto con dentro 1 2 valori name e house
+    return name, house #in questo caso stiamo tornando una tuple, un unico oggetto con dentro 1 2 valori name e house è immutabile
+    # le parentesi nel return non sono obbligatorie le mettiamo per chiarezza
 
+#possiamo refactorarla come segue:
+def main2():
+    student = get_student()
+    print(f"{student[0]} from {student[1]}") # so gia il valore di ritorno della tupla, posso prenderli come una lista
 if __name__ == "__main__":
     main()
