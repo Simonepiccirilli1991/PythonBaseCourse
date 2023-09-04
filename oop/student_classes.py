9fd39c480081f5a8725450af42d4caaaef861366
Simonepiccirilli1991 <Kuugo91@gmail.com>
2023-09-04 23:57:41 +0200

    to string on class added

--- oop/student_classes.py
+++ oop/student_classes.py
@@ -6,11 +6,14 @@ class Student:  #<- in py dentro le classi devi definire il costruttore come sot
             raise ValueError("Invalid house")
         self.name = name
         self.house = house
+# __str__ serve a fare il toString custom volendo, viene chiamata di defualt se vuoi prontare oggetto
+    def __str__(self):
+        return f"{self.name} from {self.house}"
 
 
 def main():
     student = get_student()
-    print(f"{student.name} from {student.house}")
+    print(student)
 
 
 def get_student():
