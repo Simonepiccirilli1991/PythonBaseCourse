a3bf4c11bb0729383cfd937a9f5c93ebfa08b5bd
Simonepiccirilli1991 <Kuugo91@gmail.com>
2023-09-12 20:33:16 +0200

    classMethod with singleton

---
+++ oop/hat.py
@@ -0,0 +1,14 @@
+import random
+
+
+class Hat:
+
+    house = ["Gryffindor, Slytherin", "Hafflepuff", "Ravenclaw"]#variabile di classe, accesibile alla classe
+
+    @classmethod
+    def sort(cls, name): #passiamo cls perche si riferisce alla classe e non a self istanza
+        print(name, "is in", random.choice(cls.house))
+
+
+Hat.sort("Harry") # per chiamare il metodo della classe mi riferisco al nome della classe stessa
+
